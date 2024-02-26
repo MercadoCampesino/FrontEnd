a9cb7d61f929c420c7ea800f73943b1e425fc5be
cristian1016 <cpulgarinsalgado@gmail.com>
2024-02-25 21:15:01 -0500

    I added a search engine to the discounted products

--- src/Components/ProductCard/DiscountedProducts.jsx
+++ src/Components/ProductCard/DiscountedProducts.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
 import productsDiscount from '../../assets/Products/productsDiscount';
-import './DiscountedProducts.css';
+import './DiscountedProducts.css'
 
-function DiscountedProducts({ searchTerm }) {
-  // Filtrar la lista de productos según el término de búsqueda
-  const filteredProducts = productsDiscount.filter((product) =>
-    product.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+function DiscountedProducts({ showAll }) {
+
+  const productsToShow = showAll ? productsDiscount : productsDiscount.slice(0, 4);
 
   return (
     <>
-      {filteredProducts.map((product) => (
+      {productsToShow.map((product) => (
         <div className='card_products' key={product.id}>
-          <img className='images' src={product.image} alt='' />
-          <p className='name_products'>{product.name}</p>
+          <img className='images' src={product.image} alt="" />
+            <p className='name_products'>{product.name}</p>
           <div className='name_price_product'>
-            <p className='price_discount'>
-              <strong>Ahora: </strong>
-              <em>$</em> {product.priceDiscount}
-            </p>
-            <p>
-              <strong>Antes: </strong>
-              <span className='price_products_'>
-                <em>$</em> {product.price}
-              </span>
-            </p>
+            <p className='price_discount'><strong>Ahora: </strong><em>$</em> {product.priceDiscount}</p>
+            <p><strong>Antes: </strong><span className='price_products_'> <em>$</em> {product.price}</span></p>
           </div>
-          <input className='buys' type='submit' value='Comprar' />
+          <input className='buys' type="submit" value="Comprar" />
         </div>
       ))}
     </>
@@ -34,37 +24,3 @@ function DiscountedProducts({ searchTerm }) {
 }
 
 export default DiscountedProducts;
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import productsDiscount from '../../assets/Products/productsDiscount';
-// import './DiscountedProducts.css'
-
-// function DiscountedProducts({ showAll }) {
-
-//   const productsToShow = showAll ? productsDiscount : productsDiscount.slice(0, 4);
-
-//   return (
-//     <>
-//       {productsToShow.map((product) => (
-//         <div className='card_products' key={product.id}>
-//           <img className='images' src={product.image} alt="" />
-//             <p className='name_products'>{product.name}</p>
-//           <div className='name_price_product'>
-//             <p className='price_discount'><strong>Ahora: </strong><em>$</em> {product.priceDiscount}</p>
-//             <p><strong>Antes: </strong><span className='price_products_'> <em>$</em> {product.price}</span></p>
-//           </div>
-//           <input className='buys' type="submit" value="Comprar" />
-//         </div>
-//       ))}
-//     </>
-//   );
-// }
-
-// export default DiscountedProducts;
