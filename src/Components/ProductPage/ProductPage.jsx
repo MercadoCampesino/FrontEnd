c2345c491d0afbf47a94dfced89990e880addefb
cristian1016 <cpulgarinsalgado@gmail.com>
2024-04-03 08:53:19 -0500

    responsive ProductPage

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -5,6 +5,7 @@ import { CartIcon } from "../Icon"
 import "./ProductPage.css"
 import { SingleProductCard } from "../SingleProductCard/SingleProductCard"
 import Header from "../Header/Header"
+
 export const ProductPage = () => {
     const { productId } = useParams()
     const [product, setProduct] = useState({})
@@ -64,10 +65,10 @@ export const ProductPage = () => {
             <Header />
             <main className="product-main">
                 <header className="base-product-presentation">
-                <div className='hojas'>
-                    <img className='hojasIzquierdabig' src="/images/hojasizqDesc.png" alt="" width={250} height={350} />
-                    <img className='hojasDerechabig' src="/images/hojasderDescbig.png" alt="" width={130} height={315} />
-                </div>
+                    <div className='hojas'>
+                        <img className='hojasIzquierdabig' src="/images/hojasizqDesc.png" alt="" width={250} height={350} />
+                        <img className='hojasDerechabig' src="/images/hojasderDescbig.png" alt="" width={130} height={315} />
+                    </div>
                     <h2>Compra los mejores productos</h2>
                     <h3>Sabores que cuentan historias</h3>
                 </header>
@@ -79,35 +80,38 @@ export const ProductPage = () => {
                     </picture>
                     <div className="product-info">
                         <h2>{product.nombre}</h2>
-                        {/* <hr className="hrrr"/> */}
+
                         <section className="tienda-info">
                             <picture>
                                 <img src={tienda.imagen} alt={tienda.nombre} />
                             </picture>
+
                             <p>publicado por <em>{tienda.nombre}</em></p>
                         </section>
+
                         <p>Precio: ${product.precio}</p>
+
                         <button className="add-to-cart-button">
                             Agregar
                             <CartIcon />
                         </button>
-
                     </div>
                 </article>
+
                 <section className="related-products">
                     <h2>Productos que te podrian interesar</h2>
                     <p>Mira un poco más alla, descubre nuevas cosas</p>
+
                     <div className="related-products-container">
                         <div className='hojas'>
                             <img className='hojasIzquierdabig' src="/images/hojasizqDesc.png" alt="" width={250} height={350} />
                             <img className='hojasDerechabig' src="/images/hojasderDescbig.png" alt="" width={130} height={315} />
                         </div>
+
                         {products.map((p) => (
                             <SingleProductCard key={p.idProducto} {...p} />
                         ))}
-
                     </div>
-
                 </section>
             </main>
         </>
